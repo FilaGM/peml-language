e4caca477a1b38d8982d2766447fa1f4715fccad
FilaGM <fila.heger@gmail.com>
2020-11-24 11:49:00 +0100

    commit

--- index.py
+++ index.py
@@ -3,18 +3,11 @@ import tkinter as tk
 #==form init window1==
 window1 = tk.Tk()
 window1.geometry("500x500")
-window1.title("Window")
+window1.title("window1")
 #==form init window1==
 
-Title = tk.Label(window1,text="Title")
-Title.place(x="0",y="0")
-
-Input1 = tk.Entry(window1)
-Input1.pack()
-
-#~script source~ START
-print("Form started")
-#~script source~ END
+sometext = tk.Label(window1,text="Sometext")
+sometext.pack()
 
 #<looping forms>
 window1.mainloop()
